6c89520e5d3eb09f9e1e563b0e90cabdecdbc4e9
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-07-14 19:01:52 -0300

    corrigindo erro do hash

--- pynamodb/models.py
+++ pynamodb/models.py
@@ -4,11 +4,11 @@ from pynamodb.attributes import UnicodeAttribute
 class UserModel (Model):
     
     class Meta:
-        table_name = "user"
-        host = "http://localhost:8003"
-        email = UnicodeAttribute(null=True)1
-        first_name = UnicodeAttribute(null=True)
-        last_name = UnicodeAttribute(null=True)
+        table_name = "dynamodb-user"
+    host = "http://localhost:8003"
+    email = UnicodeAttribute(hash_key=True)
+    first_name = UnicodeAttribute()
+    last_name = UnicodeAttribute()
         
 if not UserModel.exists():
     UserModel.create_table(read_capacity_units=1, write_capacity_units=1, wait=True);
